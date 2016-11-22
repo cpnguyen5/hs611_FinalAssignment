b6195240861237e59e0699292e211e655da0643d
Calvin Nguyen <Calvin Nguyen>
2016-11-21 16:41:15 -0800

    Remove example disease_frequency func

--- src/final.py
+++ src/final.py
@@ -294,67 +294,6 @@ def median_age():
     return median_dict
 
 
-TABLE_NAME = "beneficiary_sample_2010"
-def disease_frequency(col):
-    """
-    Get the states in descending order of the percentage of disease claims,
-    where disease corresponds to the column name.
-
-    Parameters
-    ----------
-    col : str, unicode
-        A column name.
-
-    Returns
-    -------
-    json
-        A labeled JSON object with the state and percent disease claims out
-        of all of that state's claims.
-
-    Examples
-    --------
-    /api/v1/freq/depression
-    /api/v1/freq/diabetes
-    """
-    disease = []
-    accepted_cols = (
-        "end_stage_renal_disease",
-        "alzheimers_related_senile",
-        "heart_failure",
-        "chronic_kidney",
-        "cancer",
-        "chronic_obstructive_pulmonary",
-        "depression",
-        "diabetes",
-        "ischemic_heart",
-        "osteoporosis",
-        "rheumatoid_osteo_arthritis",
-        "stroke_ischemic_attack",
-    )
-    # Strip the user input to alpha characters only
-    cleaned_col = re.sub('\W+', '', col)
-    try:
-        if cleaned_col not in accepted_cols:
-            raise AssertionError("Column '{0}' is not allowed".format(cleaned_col))
-        con, cur = cursor_connect(psycopg2.extras.DictCursor)
-        query = """
-        SELECT state, {1}/claims::float AS frequency FROM (SELECT
-        LHS.state AS state, {1}, claims FROM (SELECT state, count(*) AS
-        claims FROM {0} GROUP BY state order by claims desc)
-        AS LHS LEFT JOIN (SELECT state, count(*) AS {1} FROM
-        {0} WHERE {1}='true' GROUP BY state) AS RHS
-        ON LHS.state=RHS.state) AS outer_q
-        ORDER by frequency DESC;""".format(TABLE_NAME, cleaned_col)
-        cur.execute(query)
-        result = cur.fetchall()
-        for row in result:
-            freq = {row['state']: row['frequency']}
-            disease.append(freq)
-    except Exception as e:
-        raise Exception("Error: {}".format(e.message))
-    return disease
-
-
 if __name__ == "__main__":
     cursor_connect()
     # print disease_frequency('diabetes')
